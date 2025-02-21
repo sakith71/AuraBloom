06997c92ad65617c0754dd7233f13dc302a6901d
sakith <sakiththewmika71@gmail.com>
2025-02-22 01:40:29 +0530

    Added SplashScreen
    Added splash screen with gradient background and logo

---
+++ frontend/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/SplashScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AuraBloom',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color(0xFFD4C0D6)
+        // Set other theme properties as needed
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ frontend/lib/screens/splashScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+// import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFF4C2CA), // Light Pink
+              Color(0xFFD4C0D6), // Light Purple
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Welcome to AuraBloom',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Image(
+                image: AssetImage('assets/logo.png'),
+                height: 100,
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
