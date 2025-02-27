dd311ef6198e621f2d49090848e23cc3187fc59a
Modisa Nallaperuma <modisa.20230363@iit.ac.lk>
2025-02-28 01:40:14 +0530

    Connect firebase with flutter

---
+++ frontend/.dart_tool/flutter_build/6bbf493da58f14f19c228ed368dba555/dart_build.d
@@ -0,0 +1,1 @@
+ C:\\Users\\modis\\OneDrive\\Documents\\GitHub\\AuraBloom\\frontend\\.dart_tool\\flutter_build\\6bbf493da58f14f19c228ed368dba555\\dart_build_result.json: 
---
+++ frontend/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'screens/splash-screen.dart';
+import 'screens/login-page.dart';
+import 'screens/signup-screen.dart';
+import 'screens/home-screen.dart';
+import 'screens/personal-info-screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AuraBloom',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: const Color(0xFFD4C0D6)
+        // Set other theme properties as needed
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/home': (context) => const HomeScreen(selectedDates: {}),
+        '/personal-info': (context) => const PersonalInfoScreen(),
+      },
+    );
+  }
+}
