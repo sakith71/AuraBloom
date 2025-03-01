7c69ee8bf1e5b233525764335040302884cb094e
Modisa Nallaperuma <modisa.20230363@iit.ac.lk>
2025-03-01 13:00:54 +0530

    Implemented authentication service using Firebase
    - Added AuthService class for handling authentication operations.
    - Implemented signUp method to create a user with email and password.
    - Implemented signInWithEmail method for user authentication.
    - Implemented signOut method to log users out.
    - Used Firebase Authentication for secure user management.
    - Exception handling added for sign-up and sign-in processes.

---
+++ frontend/lib/services/auth-service.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign up with email and password
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      throw Exception('Failed to sign up: $e');
+      return null;
+    }
+  }
+
+  //Sign in with email and password
+  Future<User?> signInWithEmail(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      throw Exception('Failed to sign in: $e');
+      return null;
+    }
+  }
+
+  //Sign out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
